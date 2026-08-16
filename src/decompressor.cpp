baa76e59e7326ff08c77e7465cc7b55d73b63f2c
Chris <morenochristopher580@gmail.com>
2026-08-16 16:33:13 -0500

    Remove utf8_to_wstring function from decompressor
    Removed the utf8_to_wstring function for converting UTF-8 to wide string.

--- src/decompressor.cpp
+++ src/decompressor.cpp
@@ -1,5 +1,6 @@
 #include "nekoarchive/decompressor.h"
 #include "nekoarchive/aes_custom.h"
+#include "nekoarchive/unicode.h"
 #include <zstd.h>
 #include <lzma.h>
 #include <xxhash.h>
@@ -16,16 +17,6 @@
 #include <windows.h>
 #include <codecvt>
 #include <locale>
-
-// Convert UTF-8 string to wide string (Windows UTF-16)
-std::wstring utf8_to_wstring(const std::string& str) {
-    if (str.empty()) return std::wstring();
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), NULL, 0);
-    std::wstring wstr(size_needed, 0);
-    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &wstr[0], size_needed);
-    return wstr;
-}
-#endif
 
 namespace NekoArchive {
 
