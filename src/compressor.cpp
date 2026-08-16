229bbd41de2d4fba2644b70d0fe62bdf7f501493
Chris <morenochristopher580@gmail.com>
2026-08-16 10:05:24 -0500

    Add AES encryption functions to compressor.cpp
    Added AES encryption function declarations for use in compression.

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -12,6 +12,13 @@
 #include <fstream>
 #include <algorithm>
 
+// pls work
+extern "C" {
+    void AES_init_ctx(struct AES_ctx* ctx, const unsigned char* key);
+    void AES_ECB_encrypt(struct AES_ctx* ctx, unsigned char* buf);
+    void AES_ECB_decrypt(struct AES_ctx* ctx, unsigned char* buf);
+}
+
 namespace NekoArchive {
 
 struct Compressor::Impl {
