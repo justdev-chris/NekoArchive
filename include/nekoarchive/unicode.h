c454682ce73e4e2ef76c567df5c2ae17bbc93051
Chris <morenochristopher580@gmail.com>
2026-08-16 16:31:00 -0500

    Add UTF-8 to wide string conversion function

---
+++ include/nekoarchive/unicode.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+#ifdef _WIN32
+#include <windows.h>
+
+// Convert UTF-8 string to wide string (Windows UTF-16)
+inline std::wstring utf8_to_wstring(const std::string& str) {
+    if (str.empty()) return std::wstring();
+    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), NULL, 0);
+    std::wstring wstr(size_needed, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &wstr[0], size_needed);
+    return wstr;
+}
+#endif
