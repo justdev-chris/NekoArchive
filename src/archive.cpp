2235ed51c684310c8dc26fd2392e6a9a043d957d
Chris <morenochristopher580@gmail.com>
2026-08-16 16:32:04 -0500

    Remove utf8_to_wstring function
    Removed the utf8_to_wstring function for converting UTF-8 strings to wide strings.

--- src/archive.cpp
+++ src/archive.cpp
@@ -1,6 +1,7 @@
 #include "nekoarchive/archive.h"
 #include "nekoarchive/compressor.h"
 #include "nekoarchive/decompressor.h"
+#include "nekoarchive/unicode.h"
 #include <fstream>
 #include <filesystem>
 #include <zstd.h>
@@ -12,16 +13,6 @@
 #include <windows.h>
 #include <codecvt>
 #include <locale>
-
-// Convert UTF-8 string to wide string (Windows UTF-16)
-std::wstring utf8_to_wstring(const std::string& str) {
-    if (str.empty()) return std::wstring();
-    int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), NULL, 0);
-    std::wstring wstr(size_needed, 0);
-    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &wstr[0], size_needed);
-    return wstr;
-}
-#endif
 
 namespace fs = std::filesystem;
 
